fef04051eb75783b6c443c53f5eca2eaaab7aa88
pedrotakahashi <pedroh.takahashi@gmail.com>
2022-08-24 16:57:44 -0300

    Projeto Inicial

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return <h2>oi</h2>;
+}
+
+export default App;
---
+++ src/Components/Pages/index.tsx
@@ -0,0 +1,5 @@
+const Component = () => {
+  return <h4>componente da pagina</h4>;
+};
+
+export default Component;
